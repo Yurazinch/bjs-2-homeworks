f0457ac25d02c30683403766630a4188014f51ea
Yuriy Zinchenko <yurazinch@gmail.com>
2025-01-13 20:33:42 +0300

    Init commit - задание 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+
+  let diskriminant = b ** 2 - 4 * a * c;
+
+  if (diskriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else if (diskriminant > 0) {
+    arr.push((-b + Math.sqrt(diskriminant) )/(2 * a));
+    arr.push((-b - Math.sqrt(diskriminant) )/(2 * a));
+  }
   
   return arr;
 }
