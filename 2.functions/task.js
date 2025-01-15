2662d289c2b8eb08054de7fb3492b40dbe92c071
Yuriy Zinchenko <yurazinch@gmail.com>
2025-01-15 14:30:16 +0300

    Тема 2. Задание 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,20 @@
 function getArrayParams(...arr) {
 
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr [i];
+    }
+    sum = sum + arr[i];
+  }
+  let avgFull = sum / arr.length;
+  avg = parseFloat(avgFull.toFixed(2));
+
   return { min: min, max: max, avg: avg };
 }
 
