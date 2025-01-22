92d189e6714a1df7f085bdbd2cd5b17c2c72b6e2
Yuriy Zinchenko <yurazinch@gmail.com>
2025-01-22 10:55:24 +0300

    Задание objects ошибка блокировки добавления marks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,29 @@
 function Student(name, gender, age) {
-  
+     this.name = name;
+     this.gender = gender;
+     this.age = age;
+     this.marks = [];
 }
+
+let studentIvan = new Student('Иван', 'мужской', 19);
 
 Student.prototype.setSubject = function (subjectName) {
-  
+   this.subject = subjectName;  
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marks) {       
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length < 1 || this.marks === undefined) {
+      return 0;
+    }
+    return this.marks.reduce((sum, mark) => sum + mark, 0) / this.marks.length;      
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
