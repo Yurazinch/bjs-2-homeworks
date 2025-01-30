c9e75008886a32867e40a1b0befa44539b321113
Yuriy Zinchenko <yurazinch@gmail.com>
2025-01-30 19:55:23 +0300

    Задание exception-closure 2 отладка

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,3 +15,49 @@ function validateCount(value) {
     }    
 }
 
+class Triangle {
+    constructor (a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    validateTriangle (a, b, c) {
+        if (a + b <= c || b + c <= a || c + a <= b) {
+            throw new Error('Ошибка! Треугольника не существует');
+        }
+    }
+
+    get perimeter () {
+        this.validateTriangle(a,b,c);
+        try {
+            return this._perimeter = this.a + this.b + this.c;
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    get area () {
+        this.validateTriangle(a,b,c);
+        try {
+            let p = (this.a + this.b +this.c) / 2;
+            return this._area = Math.floor(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)), 3);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    } 
+
+    getTriangle (a, b, c) {
+        return {
+            'Длинна а': a,
+            'Длина b': b,
+            'Длина с': c,
+            'Периметр': this.perimeter,
+            'Площадь': this.area,
+        }
+        
+    }
+}
+
