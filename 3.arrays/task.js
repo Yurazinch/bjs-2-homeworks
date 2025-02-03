d1f48a60159bf30c8e3bd40beee503015921e704
Yuriy Zinchenko <yurazinch@gmail.com>
2025-02-03 12:29:43 +0300

    Задание arrays OK

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,24 +3,24 @@ function compareArrays(arr1, arr2) {
 }
 
 //вариант1
-/*function getUsersNamesInAgeRange(users, gender) {
+function getUsersNamesInAgeRange(users, gender) {
   if (users.length < 1 || (gender != 'мужской' && gender != 'женский')) {
     return 0;
   } 
   let ages = users.filter(element => element.gender === gender).map(element => element.age);
   let ageTotal = ages.reduce((total, item) => total + item, 0);
   return ageTotal / ages.length;
-}*/
+}
 
 //вариант 2
-function getUsersNamesInAgeRange(users, gender) {
+/*function getUsersNamesInAgeRange(users, gender) {
   if (users.length < 1 || (gender != 'мужской' && gender != 'женский')) {
     return 0;
   } 
   users.filter(element => element.gender === gender).map(element => element.age).reduce((total, item, index, array) => {
-    total += item;
+    total + item;
     if (index === array.length - 1) {
       return total / array.length;
     }
   }, 0);
-}  
+}  */
