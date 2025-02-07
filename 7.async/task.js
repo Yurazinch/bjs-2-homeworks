c194ec80311eeaed374ee029106775fc69d5ae17
Yuriy Zinchenko <yurazinch@gmail.com>
2025-02-07 21:54:24 +0300

    Задание async 3 пункта ошибки

---
+++ 7.async/task.js
@@ -0,0 +1,65 @@
+class AlarmClock {
+    constructor (alarmCollection, intervalId) {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    addClock(time, callback) {             
+        if(time === null || callback === undefined) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+        for (let i of this.alarmCollection) {        
+            if(this.alarmCollection[i].time === time) {
+            console.warn('Уже присутствует звонок на это же время');
+            }
+        }
+        let arg = {
+            time: time,
+            callback: callback,
+            canCall: true
+        }
+        this.alarmCollection.push(arg);
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(element => element.time != time);
+    }
+
+    getCurrentFormattedTime() {        
+        return new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+    }
+
+    start() {
+        if (this.intervalId) {
+            return;
+        }
+        this.intervalId = setInterval(() => {
+            this.alarmCollection.forEach(item => { 
+                if((item.time === this.getCurrentFormattedTime()) &&
+                (item.canCall === true)) {
+                    item.canCall = false;
+                    item.callback();
+                }
+            })
+        }, 1000)
+    }                
+    
+
+    stop() {
+        clearInterval(this.intervalId);
+        if(this.intervalId) {
+            this.intervalId = null;
+        }
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(item => item.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
